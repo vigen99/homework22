faf4a30d676a013eced6809576df33460330fdb5
vigen99 <NAZARYAN.VIGEN.99.VN@GMAIL.COM>
2017-09-13 12:51:27 +0400

    first commit

---
+++ homework2.js
@@ -0,0 +1,7 @@
+var rows = prompt("How many rows for your multiplication table?");
+var cols = prompt("How many columns for your multiplication table?");
+
+if(rows == "" || rows == null) {
+
+	rows = 10;
+}
